93d79479d23c80f45cc2356a481ed21dfd3e35ff
KiviaThais <kiviathais@gmail.com>
2023-09-26 16:38:55 -0300

    findAll para chamados

--- src/main/java/com/kivia/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/kivia/helpdesk/resources/ChamadoResource.java
@@ -1,5 +1,7 @@
 package com.kivia.helpdesk.resources;
 
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +13,10 @@ import com.kivia.helpdesk.domain.Chamado;
 import com.kivia.helpdesk.domain.dtos.ChamadoDTO;
 import com.kivia.helpdesk.services.ChamadoService;
 
+import jakarta.validation.constraints.AssertFalse.List;
+
 @RestController
-@RequestMapping (value = "/chamados")
+@RequestMapping(value = "/chamados")
 public class ChamadoResource {
 	
 	@Autowired
@@ -22,5 +26,12 @@ public class ChamadoResource {
 	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
 		Chamado obj = service.findById(id);
 		return ResponseEntity.ok().body(new ChamadoDTO(obj));
+	}
+	
+	@GetMapping
+	public ResponseEntity<java.util.List<ChamadoDTO>> findAll(){
+		java.util.List<Chamado> list = service.findAll();
+		java.util.List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
