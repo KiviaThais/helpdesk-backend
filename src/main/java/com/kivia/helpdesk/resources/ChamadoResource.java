90aeeeb4ecf45e65df8efaab18804764b4203884
KiviaThais <kiviathais@gmail.com>
2023-09-27 15:17:15 -0300

    create para chamado

--- src/main/java/com/kivia/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/kivia/helpdesk/resources/ChamadoResource.java
@@ -1,19 +1,23 @@
 package com.kivia.helpdesk.resources;
 
+import java.net.URI;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.kivia.helpdesk.domain.Chamado;
 import com.kivia.helpdesk.domain.dtos.ChamadoDTO;
 import com.kivia.helpdesk.services.ChamadoService;
 
-import jakarta.validation.constraints.AssertFalse.List;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/chamados")
@@ -34,4 +38,23 @@ public class ChamadoResource {
 		java.util.List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
+	
+	@PostMapping
+	public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO){
+		Chamado obj = service.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/kivia/helpdesk/services/ChamadoService.java
+++ src/main/java/com/kivia/helpdesk/services/ChamadoService.java
@@ -7,14 +7,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kivia.helpdesk.domain.Chamado;
+import com.kivia.helpdesk.domain.Cliente;
+import com.kivia.helpdesk.domain.Tecnico;
+import com.kivia.helpdesk.domain.dtos.ChamadoDTO;
+import com.kivia.helpdesk.domain.enums.Prioridade;
+import com.kivia.helpdesk.domain.enums.Status;
 import com.kivia.helpdesk.repositories.ChamadoRepository;
 import com.kivia.helpdesk.services.exceptions.ObjectnotFoundException;
+
+import jakarta.validation.Valid;
 
 @Service
 public class ChamadoService {
 
 	@Autowired
 	private ChamadoRepository repository;
+	@Autowired
+	private TecnicoService tecnicoService;
+	@Autowired
+	private ClienteService clienteService;
 	
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
@@ -24,4 +35,33 @@ public class ChamadoService {
 	public List<Chamado> findAll() {
 		return repository.findAll();
 	}
+
+	public Chamado create(@Valid ChamadoDTO objDTO) {
+		return repository.save(newChamado(objDTO));
+	}
+	
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+		
+		Chamado chamado = new Chamado();
+		if(obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		
+		return chamado;
+	}
+	
 }
+
+
+
+
+
